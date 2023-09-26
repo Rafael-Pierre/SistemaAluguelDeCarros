a636915796f368cb4bbd5297284e447b66bfd567
Rafael Pierre <pierredoc28@gmail.com>
2023-09-26 19:52:25 -0300

    teste

--- code/frontend/src/pages/login.tsx
+++ code/frontend/src/pages/login.tsx
@@ -1,54 +1,72 @@
 import { Button, Checkbox, Form, Input } from "antd";
 import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const onFinish = async (values: any) => {
-  console.log("Success:", values);
+const App: React.FC = () => {
+  const router = useRouter();
+  const OnFinish = async (values: any) => {
+    try {
+      const response = await axios.post("http://localhost:5500/auth", values);
 
-  try {
-    const response = await axios.post("http://localhost:5500/auth", values);
+      localStorage.setItem("token", response.data.access_token);
 
-    localStorage.setItem("token", response.data.access_token);
-    return response.data;
-  } catch (error) {
-    console.error("Error:", error);
-  }
-};
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      };
 
-const onFinishFailed = (errorInfo: any) => {
-  console.log("Failed:", errorInfo);
-};
+      const userInfo: any = await axios.get("http://localhost:5500/auth/info", {
+        headers,
+      });
 
-type FieldType = {
-  email?: string;
-  password?: string;
-};
+      console.log(userInfo);
+      if (userInfo.data.role === "client") {
+        router.push(`/${userInfo.data.role}`);
+      } else {
+        router.push(`/agente`);
+      }
 
-const App: React.FC = () => (
+      return response.data;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  type FieldType = {
+    email?: string;
+    password?: string;
+  };
+
+  return (
     <div className="forms">
       <h1 className="title">Faça seu Login:</h1>
 
       <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={OnFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
         <Form.Item<FieldType>
-            label="email"
-            name="email"
-            rules={[{ required: true, message: "Please input your username!" }]}
+          label="email"
+          name="email"
+          rules={[{ required: true, message: "Please input your username!" }]}
         >
           <Input />
         </Form.Item>
         <Form.Item<FieldType>
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
         >
           <Input.Password />
         </Form.Item>
@@ -64,6 +82,7 @@ const App: React.FC = () => (
         </div>
       </Form>
     </div>
-);
+  );
+};
 
 export default App;
